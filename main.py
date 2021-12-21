dcd3b0d717f1d69c6496258de061b159266f329b
Sergei Bahshchikov <s.o.banshchikov@gmail.com>
2021-12-21 17:35:19 +0200

    second player can join

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.requests import Request
 from typing import List, Dict
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,13 +19,18 @@ class ConnectionManager:
     def __init__(self):
         self.active_connections: Dict() = {}
     
-    async def connect(self, websocket: WebSocket, client_id: int):
-        await websocket.accept()
-        print("websocket accepted", client_id)
+    async def connect(self, websocket: WebSocket, client_id: int, first_device):
+        if first_device:
+            await websocket.accept()
         
-        if self.active_connections.get(client_id, 'nope') == 'nope':
-            self.active_connections[client_id] = []
-        self.active_connections[client_id].append(websocket)
+            if self.active_connections.get(client_id, 'nope') == 'nope':
+                self.active_connections[client_id] = []
+            self.active_connections[client_id].append(websocket)
+        else:
+            if self.active_connections.get(client_id, 'nope') == 'nope':
+                raise WebSocketDisconnect
+            self.active_connections[client_id].append(websocket)
+            print("the second player is here")
     
     def disconnect(self, websocket: WebSocket, client_id: int):
         print("websocket id is", client_id)
@@ -41,9 +47,9 @@ manager = ConnectionManager()
 async def get_game(request: Request):
     return templates.TemplateResponse("board.html", {"request": request})
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket, client_id)
+@app.websocket("/ws/{client_id}/{first_device}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int, first_device: bool):
+    await manager.connect(websocket, client_id, first_device)
     try:
         while True:
             data = await websocket.receive_text()
@@ -52,3 +58,11 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
         print("websocket closed", client_id)
         manager.disconnect(websocket, client_id)
         await manager.send_message(f'Player {client_id} has left', client_id)
+
+
+# class Id(BaseModel):
+#     connection_number: int
+
+# @app.post("/join")
+# async def join(id: Id):
+#     return manager.join(id)
--- static/index.js
+++ static/index.js
@@ -74,22 +74,23 @@ document.getElementById('sameDeviceBtn').onclick = function() {
     for (let key of Object.keys(defaultBoardState)) {
         boardState[key] = defaultBoardState[key]
     }
-    if (ws) { ws.close(); }
+    // if (ws.keys() !== 0) { ws.close(); }
     repaintBoard(defaultBoardState);
     gameMenu.classList.add("invisible", "underlayer");
 }
 
 
 
-document.getElementById('differentDevicesBtn').onclick = function() {
+document.getElementById('firstDevicesBtn').onclick = function() {
 
     const getRandomId = max => {
         return Math.floor(Math.random() * max);
     }
     
     let client_id = getRandomId(100000);
+    client_id = 1;
     document.querySelector("#ws-id").textContent = client_id;
-    ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+    ws = new WebSocket(`ws://localhost:8000/ws/${client_id}/${true}`);
     
     ws.onmessage = function(event) {
         const newState = JSON.parse(event.data);
@@ -99,13 +100,29 @@ document.getElementById('differentDevicesBtn').onclick = function() {
         repaintBoard(boardState); 
     
     };
-
     boardState.isRemote = true;
     document.getElementsByClassName('invitation')[0].classList.remove('invisible');
-
 }
 
 document.getElementsByClassName('toggle-game-menu')[0].onclick = function() {
     gameMenu.classList.remove("invisible", "underlayer");
 }
 
+document.forms.join.onsubmit = function(e) {
+        const client_id = this.joinId.value;
+    // if (ws) { ws.close(); }
+    ws = new WebSocket(`ws://localhost:8000/ws/${client_id}/${true}`);
+    boardState.isRemote = true;
+    gameMenu.classList.add('invisible', 'underlayer');
+
+    ws.onmessage = function(event) {
+        const newState = JSON.parse(event.data);
+        for (let key of Object.keys(boardState)) {
+            boardState[key] = newState[key]
+        }
+        repaintBoard(boardState); 
+    
+    };
+
+    e.preventDefault();
+}
